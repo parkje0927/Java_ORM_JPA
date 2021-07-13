ae09231acd0e0371f163712f61ef055068d548c4
jh <parkje09270@gmail.com>
2021-07-13 12:05:25 +0900

    2. Hello JPA - 애플리케이션 개발

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
@@ -17,13 +18,51 @@ public class JpaMain {
         /**
          * code
          */
-        Member member = new Member();
-        member.setId(1L);
-        member.setName("HelloA");
-        em.persist(member); //transaction 안에서 이루어져야 member 가 저장된다.
+        try {
+            //등록
+//            Member member = new Member();
+//            member.setId(2L);
+//            member.setName("HelloB");
+//            em.persist(member); //transaction 안에서 이루어져야 member 가 저장된다.
+//
+//            tx.commit();
 
-        tx.commit();
-        em.close();
+            //조회
+//            Member findMember = em.find(Member.class, 1L);
+//            System.out.println("findMember.id() = " + findMember.getId());
+//            System.out.println("findMember.name() = " + findMember.getName());
+//            tx.commit();
+
+            //삭제
+//            Member findMember = em.find(Member.class, 1L);
+//            em.remove(findMember);
+//            tx.commit();
+
+            //수정
+//            Member findMember = em.find(Member.class, 1L);
+//            findMember.setName("HelloJPA");
+            /*
+            em.persist(findMember); 를 안해도 된다.
+            jpa 가 관리를 하면서 변경이 되었는지 commit 시점에서 체크를 해서 변경 사항이 있으면 update query 가 나간다.
+             */
+//            tx.commit();
+            
+            //JPQL 
+            //대상이 테이블이 아니고 객체이다.
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+//                    .setFirstResult(5) /* pagination */
+//                    .setMaxResults(8) /* pagination */
+                    .getResultList();
+            for (Member member : result) {
+                System.out.println("member.getName() = " + member.getName());
+            }
+            tx.commit();
+
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
         emf.close();
     }
 }
